ca5614f1ce4b9bc9be30ac041f3ab81e190dae4e
Macbook Air <macbookair@Macbooks-Air>
2024-08-24 00:42:39 +0100

    Advent calender list

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+//const days=[1,6,7,8,9,6];
+const days = [...Array(24)].map((val,index)=>index +1);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>#DEVember 2024</Text>
+<View style={styles.container}>
+
+
+
+    {
+    
+    <FlatList 
+      data={days}
+      numColumns={2}
+      contentContainerStyle={styles.content}
+      columnWrapperStyle={styles.column}
+      renderItem={({item}) => ( <DayListItem day={item} />
+        
+      )}
+    
+    />
+    
+    /*days.map((day)=> (
+      <View style={styles.box} key={day}> 
+      <Text style={styles.text}>{day}</Text>
+    </View>
+    ))*/}
+
+    
+      
+    
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +44,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    
+  },
+  column:{
+    gap:10,
+  },
+  box:{
+    backgroundColor:'#f9ede3',
+    flex:1,
+    aspectRatio:1,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#9b4521',
+    borderRadius:20,
+
+    justifyContent:'center',
+    alignItems:'center',
+    
+  },
+  content:{
+    gap:10,
+  },
+  text:{
+    color:'#9b4521',
+    fontSize: 70,
   },
 });
---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,35 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+type DayListItem = {
+    day:number;
+}
+
+export default function DayListItem({day}: DayListItem){
+    return(
+        <View style={styles.box}> 
+        <Text style={styles.text}>{day}</Text>
+      </View>
+    )
+    
+}
+
+
+const styles = StyleSheet.create({
+ box:{
+      backgroundColor:'#f9ede3',
+      flex:1,
+      aspectRatio:1,
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: '#9b4521',
+      borderRadius:20,
+  
+      justifyContent:'center',
+      alignItems:'center',
+      
+    },
+text:{
+      color:'#9b4521',
+      fontSize: 70,
+    },
+  });
+  
