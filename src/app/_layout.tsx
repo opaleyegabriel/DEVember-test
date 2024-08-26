d8eb106f64ff643c4f1989c9a05accfdadb5872b
Macbook Air <macbookair@Macbooks-Air>
2024-08-27 00:22:52 +0100

    stack navigator

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+
+export default function RootLayout(){
+    return (
+    
+    <Stack screenOptions={{title: 'DEVember',
+    headerStyle: {backgroundColor:'yellow'},
+
+
+
+}}/>);
+    
+
+}
